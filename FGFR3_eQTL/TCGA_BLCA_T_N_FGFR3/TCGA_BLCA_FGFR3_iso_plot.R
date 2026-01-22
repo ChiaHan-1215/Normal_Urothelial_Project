a2c9f30643fd1161de7198e32d567e7fadf139e5
Mila_lab_LTG <chiahan.lee@nih.gov>
2026-01-22 15:59:59 -0500

    Create TCGA_BLCA_FGFR3_iso_plot.R

---
+++ FGFR3_eQTL/TCGA_BLCA_T_N_FGFR3/TCGA_BLCA_FGFR3_iso_plot.R
@@ -0,0 +1,109 @@
+### Gaol: plotting FGFR3 isoform expression for TCGA BLCA T vs N
+
+library(vcfR)
+library(biomaRt)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(patchwork)
+
+
+
+setwd('/Volumes/ifs/DCEG/Branches/LTG/Prokunina/TCGA_data/Original source data Expression/UCSC_Toil_RNAseq_Recompute/transcript expression RNAseq_hg38/byCancerType/')
+
+# load the sample TPM count
+BLCA_N <- read.delim('TCGA_BLCA_IsoformExpression.Profile_OnlyNormal.txt')
+BLCA_T <- read.delim('TCGA_BLCA_IsoformExpression.Profile_OnlyTumor.txt')
+
+# subset FGFR3
+BLCA_N_fgfr <- BLCA_N %>% filter(gene == 'FGFR3')
+BLCA_T_fgfr <- BLCA_T %>% filter(gene == 'FGFR3')
+
+# As the TPM value is transfomed, need to reconvert to TPM
+#log2(tpm+0.001)
+
+BLCA_N_fgfr[, 7:ncol(BLCA_N_fgfr)] <- apply(BLCA_N_fgfr[, 7:ncol(BLCA_N_fgfr)], 
+                                          MARGIN = c(1, 2), 
+                                          FUN = function(x) round(2^x - 0.001, 2))
+
+BLCA_T_fgfr[, 7:ncol(BLCA_T_fgfr)] <- apply(BLCA_T_fgfr[, 7:ncol(BLCA_T_fgfr)], 
+                                          MARGIN = c(1, 2), 
+                                          FUN = function(x) round(2^x - 0.001, 2))
+
+
+# For Normal
+BLCA_N_fgfr <- data.frame(t(BLCA_N_fgfr))
+names(BLCA_N_fgfr) <-BLCA_N_fgfr[1,] 
+BLCA_N_fgfr <- BLCA_N_fgfr[-c(1:6),]
+
+BLCA_N_fgfr <- BLCA_N_fgfr %>%
+  mutate(across(everything(), as.numeric))
+
+BLCA_N_fgfr <- BLCA_N_fgfr %>% mutate(PID=rownames(BLCA_N_fgfr),.before = 1)
+BLCA_N_fgfr$total_FGFR3 <- rowSums(BLCA_N_fgfr[,c(2:11)])
+BLCA_N_fgfr$PID <- gsub('\\.','-',BLCA_N_fgfr$PID)
+
+
+# For tumor 
+BLCA_T_fgfr <- data.frame(t(BLCA_T_fgfr))
+names(BLCA_T_fgfr) <-BLCA_T_fgfr[1,] 
+BLCA_T_fgfr <- BLCA_T_fgfr[-c(1:6),]
+
+BLCA_T_fgfr <- BLCA_T_fgfr %>%
+  mutate(across(everything(), as.numeric))
+
+BLCA_T_fgfr <- BLCA_T_fgfr %>% mutate(PID=rownames(BLCA_T_fgfr),.before = 1)
+BLCA_T_fgfr$total_FGFR3 <- rowSums(BLCA_T_fgfr[,c(2:11)])
+BLCA_T_fgfr$PID <- gsub('\\.','-',BLCA_T_fgfr$PID)
+
+
+#### Vln Box plot for isoform expression between baldder vs smooth muscle ####
+
+blca_T_long <- BLCA_T_fgfr %>%
+  pivot_longer(
+    cols = -c(PID,total_FGFR3),
+    names_to = "isoform",
+    values_to = "tpm"
+  )
+
+
+blca_N_long <- BLCA_N_fgfr %>%
+  pivot_longer(
+    cols = -c(PID,total_FGFR3),
+    names_to = "isoform",
+    values_to = "tpm"
+  )
+
+
+# Muscle plot
+p_blca_Tumor <- blca_T_long %>%
+  ggplot(aes(x = isoform, y = log2(tpm+1))) +  # Changed x to isoform
+  geom_boxplot(width = 0.4, linewidth = 0.5, colour = "black", 
+               fill = "#A8D5E2", alpha = 0.6,outlier.shape = NA) +
+  geom_jitter(width = 0.15, size = 0.1, alpha = 0.5, color = "grey30") +
+  stat_summary(fun = median, geom = "point", shape = 20, size = 2, color = "#FF0000") +
+  # Removed facet_wrap
+  theme_classic() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(color = "black", size = 10),
+        axis.text.x = element_text(color = "black", size = 9, angle = 45, hjust = 1),
+        legend.position = "none") + ylim(c(0,12)) +
+  ggtitle("Tumor (n=407)")
+
+
+p_blca_Normal <- blca_N_long %>%
+  ggplot(aes(x = isoform, y = log2(tpm+1))) +  # Changed x to isoform
+  geom_boxplot(width = 0.4, linewidth = 0.5, colour = "black", 
+               fill = "#A8D5E2", alpha = 0.6,outlier.shape = NA) +
+  geom_jitter(width = 0.15, size = 0.1, alpha = 0.5, color = "grey30") +
+  stat_summary(fun = median, geom = "point", shape = 20, size = 2, color = "#FF0000") +
+  # Removed facet_wrap
+  theme_classic() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(color = "black", size = 10),
+        axis.text.x = element_text(color = "black", size = 9, angle = 45, hjust = 1),
+        legend.position = "none") + ylim(c(0,12)) +
+  ggtitle("Normal (n=19)")
+
+
+p_blca_Normal  | p_blca_Tumor
