0f90a3eae83af736784e77f6ae026f893e53a020
Mila_lab_LTG <chiahan.lee@nih.gov>
2026-01-23 16:08:57 -0500

    Update TCGA_BLCA_FGFR3_iso_plot.R

--- FGFR3_eQTL/TCGA_BLCA_T_N_FGFR3/TCGA_BLCA_FGFR3_iso_plot.R
+++ FGFR3_eQTL/TCGA_BLCA_T_N_FGFR3/TCGA_BLCA_FGFR3_iso_plot.R
@@ -23,12 +23,12 @@ BLCA_T_fgfr <- BLCA_T %>% filter(gene == 'FGFR3')
 #log2(tpm+0.001)
 
 BLCA_N_fgfr[, 7:ncol(BLCA_N_fgfr)] <- apply(BLCA_N_fgfr[, 7:ncol(BLCA_N_fgfr)], 
-                                          MARGIN = c(1, 2), 
-                                          FUN = function(x) round(2^x - 0.001, 2))
+                                            MARGIN = c(1, 2), 
+                                            FUN = function(x) round(2^x - 0.001, 2))
 
 BLCA_T_fgfr[, 7:ncol(BLCA_T_fgfr)] <- apply(BLCA_T_fgfr[, 7:ncol(BLCA_T_fgfr)], 
-                                          MARGIN = c(1, 2), 
-                                          FUN = function(x) round(2^x - 0.001, 2))
+                                            MARGIN = c(1, 2), 
+                                            FUN = function(x) round(2^x - 0.001, 2))
 
 
 # For Normal
@@ -107,3 +107,71 @@ p_blca_Normal <- blca_N_long %>%
 
 
 p_blca_Normal  | p_blca_Tumor
+
+
+
+
+####Adding Sample SEX info
+
+
+TCGA_sex <- readxl::read_xlsx('/Volumes/ifs/DCEG/Branches/LTG/Prokunina/TCGA_data/TCGA_masterFiles/TCGA master table 2017.xlsx',sheet = 2)
+  
+TCGA_sex <- TCGA_sex[,c(1,11)]
+names(TCGA_sex)[1] <- "PID"
+
+# Now we have TN data
+BLCA_T_fgfr
+BLCA_N_fgfr
+
+
+BLCA_T_fgfr_sex <- left_join(BLCA_T_fgfr,TCGA_sex,by="PID")
+BLCA_T_fgfr_sex <- BLCA_T_fgfr_sex[,c(1,13,2:12)]
+
+BLCA_N_fgfr_sex <- left_join(BLCA_N_fgfr,TCGA_sex,by="PID")
+BLCA_N_fgfr_sex <- BLCA_N_fgfr_sex[,c(1,13,2:12)]
+
+
+# PLOT 
+
+blca_T_long_s <- BLCA_T_fgfr_sex %>%
+  pivot_longer(
+    cols = -c(PID,total_FGFR3,gender),
+    names_to = "isoform",
+    values_to = "tpm"
+  )
+
+
+blca_N_long_s <- BLCA_N_fgfr_sex %>%
+  pivot_longer(
+    cols = -c(PID,total_FGFR3,gender),
+    names_to = "isoform",
+    values_to = "tpm"
+  )
+
+
+blca_N_long_s %>%
+  ggplot(aes(x = isoform, y = log2(tpm + 1), fill = gender)) +  # 1. Added fill here
+  geom_boxplot(width = 0.6, 
+               linewidth = 0.5, 
+               colour = "black", 
+               alpha = 0.6, 
+               outlier.shape = NA,
+               position = position_dodge(width = 0.8)) + # 2. Optional: adjust spacing
+  geom_jitter(aes(group = gender),                  # 3. Group dots by sex
+              position = position_jitterdodge(jitter.width = 0.15, dodge.width = 0.8), 
+              size = 0.1, 
+              alpha = 0.3, 
+              color = "grey30") +
+  stat_summary(fun = median, 
+               geom = "point", 
+               shape = 20, 
+               size = 2, 
+               color = "#FF0000",
+               position = position_dodge(width = 0.8)) + # 4. Align red dots
+  scale_fill_manual(values = c("MALE" = "#A8D5E2", "FEMALE" = "#F2BAC9")) +
+  theme_classic() +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_text(color = "black", size = 10),
+        axis.text.x = element_text(color = "black", size = 9, angle = 45, hjust = 1),
+        legend.position = "top")
+
